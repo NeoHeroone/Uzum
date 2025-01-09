ab47324a83d7c3935cf923de9d3b53c42a88a4e8
NEO <neo.uz001@gmail.com>
2025-01-09 22:58:48 +0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -24,7 +24,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -33,7 +33,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -42,7 +42,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -51,7 +51,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -60,7 +60,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -69,7 +69,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -78,7 +78,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -87,7 +87,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
     {
       id: userId(),
@@ -96,7 +96,7 @@ const App = () => {
       bolbt: `2 975 so'm/oyiga`,
       skidka: '55 000',
       narx: `42 000 so'm`,
-      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
+      img: 'https://s3-alpha-sig.figma.com/img/c927/ce24/66ff44d86e8f35fc77d7d997da6ae440?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W-IrtsU-r7VaB1JTyavi8Epq9C~8f03H7PKpayKVuosY~L5~Jz3SNZn~LM0p1QuxAKpw7xN3oGyB2zjU7K5K6Q12Er7DX5n-kZnuinnYXKoqqj9~kCXJwwBIcvBlMmbyEdIAF6QGtV~eD2Csl9uf3Vr-1z~6BnMcHtE78mOKzqqWCwp7a9ypwrq-5igW20oCWtU-ihXl2vXuOUH07YxWma6mDipCeGZBMLzfISgG1tRPaG0-GAJSjOTOd66E6qYUkV~pzRNvWCGSxmFAEYc-RYf6JbserPGpKwqepKRI~taGAuYFm-E~KEswW8GhyIjvvO9rSTUODnWAiS4LUbO9~w__'
     },
   ]
 
@@ -106,7 +106,7 @@ const App = () => {
       <h2 className="title-box">Mashhur &#62;</h2>
       <div className="container-content">
         {data.map((item, index) => (
-          <UserCard data={item} key={index}/>
+          <UserCard data={item} key={index} />
         ))}
       </div>
     </div>
